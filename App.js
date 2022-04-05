31e05c82286d5654062f914a0455fd048ed1f58c
Kenneth <kenldurham@gmail.com>
2022-04-04 17:33:27 -0700

    first page commit

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-    <View style={styles.container}>
-      <Text>Dream team messenger</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -17,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#b2b2b2",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
